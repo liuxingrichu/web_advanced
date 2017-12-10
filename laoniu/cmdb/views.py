45266606d8a79aca711d9ed6fbc18389ab3de6be
liuxingrichu <liuxingrichu@163.com>
2017-12-10 13:13:12 +0800

    Django工程之app举例

---
+++ laoniu/cmdb/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>CMDB<h1>')
---
+++ laoniu/xiaoniu/handle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("OK")
