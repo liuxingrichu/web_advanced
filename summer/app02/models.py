668b3414bab65d12985fccde9f5de938dc073f51
liuxingrichu <liuxingrichu@163.com>
2017-12-17 18:21:38 +0800

    ORM操作：创建表结构

--- summer/app02/models.py
+++ summer/app02/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 创建的数据库表单名：app02_userinfo
+class UserInfo(models.Model):
+    # 默认创建ID列，自增， 主键
+    # 用户名列，字符串类型，指定长度
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
--- summer/app02/views.py
+++ summer/app02/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import redirect
 
 
 # Create your views here.
 
 def login(request):
-    return HttpResponse("app02 login")
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        # 在数据库中执行 select * from user where username='x' and password='x'
+        return render(request, 'login.html')
+    else:
+        return redirect("/index")
