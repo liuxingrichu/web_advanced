a6f7be247dcaf08babfa4ffa0e1b9bf68d128028
liuxingrichu <liuxingrichu@163.com>
2017-12-17 21:59:51 +0800

    ORM操作：数据库之增删改查

--- summer/app02/urls.py
+++ summer/app02/urls.py
@@ -22,4 +22,5 @@ from app02 import views
 
 urlpatterns = [
     url(r'^login', views.login),
+    url(r'^orm', views.orm),
 ]
--- summer/app02/views.py
+++ summer/app02/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.shortcuts import HttpResponse
 
 
 # Create your views here.
@@ -12,3 +13,36 @@ def login(request):
         return render(request, 'login.html')
     else:
         return redirect("/index")
+
+
+from app02 import models
+
+
+def orm(request):
+    # 增
+    models.UserInfo.objects.create(username='root', password='123')
+
+    dict_user = {'username': 'Lucy', 'password': '123'}
+    models.UserInfo.objects.create(**dict_user)
+
+    obj = models.UserInfo(username='Tom', password='123')
+    obj.save()
+
+    # 查
+    result = models.UserInfo.objects.all()
+    result = models.UserInfo.objects.filter(username='Tom')
+    # result的类型是QuerySet，是Django提供的，类似列表
+    for item in result:
+        print(item.id, item.username, item.password)
+    print(result)
+
+    # 删
+    models.UserInfo.objects.filter(username='Tom').delete()
+    models.UserInfo.objects.filter(id=1).delete()
+    models.UserInfo.objects.all().delete()
+
+    # 改
+    models.UserInfo.objects.all().update(password=999)
+    models.UserInfo.objects.filter(id=7).update(password=666)
+
+    return HttpResponse('orm')
