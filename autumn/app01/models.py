e0a3ac095d5520fa5de4c134e59a91c43c02a135
liuxingrichu <liuxingrichu@163.com>
2017-12-27 21:57:15 +0800

    基于ORM实现主机：多对多表结构

--- autumn/app01/models.py
+++ autumn/app01/models.py
@@ -14,3 +14,12 @@ class Host(models.Model):
     ip = models.GenericIPAddressField(protocol='ipv4', db_index=True)
     port = models.IntegerField()
     b = models.ForeignKey(to='Business', to_field='id')
+
+
+class Application(models.Model):
+    name = models.CharField(max_length=32)
+    r = models.ManyToManyField("Host")
+
+# class HostToApp(models.Model):
+#     hobj = models.ForeignKey(to="Host", to_field="nid")
+#     aobj = models.ForeignKey(to="Application", to_field="id")
